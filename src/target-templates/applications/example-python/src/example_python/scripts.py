0cbf45e0f76e343cb9e433c3ded877b6ec1b6e4f
Amit Aharoni <amit.sites@gmail.com>
2025-03-22 00:09:25 +0100

    Fixed python script

--- src/target-templates/applications/example-python/src/example_python/scripts.py
+++ src/target-templates/applications/example-python/src/example_python/scripts.py
@@ -3,15 +3,16 @@ from devops_python.pyproject import get_pyproject_data
 import os
 import sys 
 
+package_name = "example_python"
 data = get_pyproject_data()
 port = data.deployment["port"]
 workers = data.deployment.get("workers")
 
 def dev():
-    subprocess.run(["fastapi", "dev", "src/example_python/main.py", "--port", str(port)], check=True)
+    subprocess.run(["fastapi", "dev", f"src/{package_name}/main.py", "--port", str(port)], check=True)
 
 def start():
-    cmd = ["fastapi", "run", "src/python_mock/main.py", "--port", str(port)]
+    cmd = ["fastapi", "run", f"src/{package_name}/main.py", "--port", str(port)]
     if (workers):
         cmd.extend(["--workers", str(workers)])
 
